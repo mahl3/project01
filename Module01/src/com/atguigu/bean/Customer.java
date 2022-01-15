c742ec9445ac349ab6f40fee51a017ecc58e05c8
mahl3_glb <mahl3study@aliyun.com>
2022-01-15 22:41:05 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,13 @@
+package com.atguigu.bean;
+
+import java.util.ArrayList;
+
+public class Customer {
+    ArrayList list = new ArrayList();
+    public static void main(String args[]){
+        System.out.println("helloworld!!!");
+        System.out.println("helloworld!!!ddd");
+    }
+    public void method(){
+    }
+}
